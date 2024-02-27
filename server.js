5b27c4f3f8ef731c8c724cdbeff4d7c4298a4b41
Arléon <arleonzemtsop@gmail.com>
2024-02-27 12:30:46 +0100

    Hash and Compare Passwords Synchronously

--- server.js
+++ server.js
@@ -20,11 +20,13 @@ bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
 
 
 //END_ASYNC
-var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
-var result = bcrypt.compareSync(myPlaintextPassword, hash);
+
 //START_SYNC
 
-
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log('bcrypt.hashSync: ', hash);
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log('bcrypt.compareSync: ', result);
 
 //END_SYNC
 
